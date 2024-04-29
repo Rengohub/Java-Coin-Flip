420784ddd127c93e2777b9bc5276e1ba1d69a799
Joey Bauer <joeybauer17@gmail.com>
2024-04-29 16:41:13 -0600

    Fixed Dice Game Listeners, Refactored

--- src/client/View/ClientDiceView.java
+++ src/client/View/ClientDiceView.java
@@ -17,24 +17,10 @@ public class ClientDiceView extends JFrame {
     private JTextField betField;
     private int betAmount = 0;
     String[] imagesPath = {"src/Assets/Java-Dice-1.png", "src/Assets/Java-Dice-2.png", "src/Assets/Java-Dice-3.png", "src/Assets/Java-Dice-4.png", "src/Assets/Java-Dice-5.png", "src/Assets/Java-Dice-6.png"};
-    private String dice1 = "src/Assets/Sprites/Sprite-1.png";
-    private String dice2 = "src/Assets/Sprites/Sprite-2.png";
-    private String dice3 = "src/Assets/Sprites/Sprite-3.png";
-    private String dice4 = "src/Assets/Sprites/Sprite-4.png";
-    private String dice5 = "src/Assets/Sprites/Sprite-5.png";
-    private String dice6 = "src/Assets/Sprites/Sprite-6.png";
-    private ImageIcon icon1;
-    private ImageIcon icon2;
-    private ImageIcon icon3;
-    private ImageIcon icon4;
-    private ImageIcon icon5;
-    private ImageIcon icon6;
-    private JButton guessButton1;
-    private JButton guessButton2;
-    private JButton guessButton3;
-    private JButton guessButton4;
-    private JButton guessButton5;
-    private JButton guessButton6;
+    private ImageIcon icon[] = new ImageIcon[6];
+    private String[] spriteArr= {"src/Assets/Sprites/Sprite-1.png", "src/Assets/Sprites/Sprite-2.png", "src/Assets/Sprites/Sprite-3.png", "src/Assets/Sprites/Sprite-4.png", "src/Assets/Sprites/Sprite-5.png", "src/Assets/Sprites/Sprite-6.png"};
+    private Image img[] = new Image[6];
+    private JButton guessButton[] = new JButton[6];
     private Timer timer;
     private JButton decreaseBetButton;
     private JButton increaseBetButton;
@@ -99,52 +85,27 @@ public class ClientDiceView extends JFrame {
 
         JPanel choicesPanel = new JPanel();
 
-        icon1 = new ImageIcon(dice1);
-        icon2 = new ImageIcon(dice2);
-        icon3 = new ImageIcon(dice3);
-        icon4 = new ImageIcon(dice4);
-        icon5 = new ImageIcon(dice5);
-        icon6 = new ImageIcon(dice6);
-
         JPanel GuessPanel = new JPanel();
-        guessButton1 = new JButton();
-        guessButton2 = new JButton();
-        guessButton3 = new JButton();
-        guessButton4 = new JButton();
-        guessButton5 = new JButton();
-        guessButton6 = new JButton();
 
-        try {
-            Image img1 = ImageIO.read(new File(dice1));
-            Image img2 = ImageIO.read(new File(dice2));
-            Image img3 = ImageIO.read(new File(dice3));
-            Image img4 = ImageIO.read(new File(dice4));
-            Image img5 = ImageIO.read(new File(dice5));
-            Image img6 = ImageIO.read(new File(dice6));
 
-            icon1 = new ImageIcon(img1.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-            icon2 = new ImageIcon(img2.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-            icon3 = new ImageIcon(img3.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-            icon4 = new ImageIcon(img4.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-            icon5 = new ImageIcon(img5.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-            icon6 = new ImageIcon(img6.getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 6; i++) {
+            int number = i + 1;
+            icon[i] = new ImageIcon(spriteArr[i]);
+            guessButton[i] = new JButton(String.valueOf(number));
+            guessButton[i].addActionListener(e -> playDiceGame(number));
+
+            try {
+                img[i] = ImageIO.read(new File(spriteArr[i]));
+    
+                icon[i] = new ImageIcon(img[i].getScaledInstance(50, 50, Image.SCALE_SMOOTH));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            guessButton[i].setIcon(icon[i]);
+            GuessPanel.add(guessButton[i]);
         }
-        
-        guessButton1.setIcon(icon1);
-        guessButton2.setIcon(icon2);
-        guessButton3.setIcon(icon3);
-        guessButton4.setIcon(icon4);
-        guessButton5.setIcon(icon5);
-        guessButton6.setIcon(icon6);
 
-        GuessPanel.add(guessButton1);
-        GuessPanel.add(guessButton2);
-        GuessPanel.add(guessButton3);
-        GuessPanel.add(guessButton4);
-        GuessPanel.add(guessButton5);
-        GuessPanel.add(guessButton6);
 
         choicesPanel.add(GuessPanel);
 
@@ -174,6 +135,28 @@ public class ClientDiceView extends JFrame {
         timer = new Timer(delay, taskPerformer);
         timer.setInitialDelay(0);
         timer.start();
+    }
+
+    private void playDiceGame(int chosenNumber) {
+        if (client.getCurrentUserId() != -1) {
+            String betAmount = betAmountField.getText().trim();
+            if (betAmount.matches("\\d+") && Integer.parseInt(betAmount) > 0) {
+                disableButtons();
+                String requestData = String.format("%d,%s,%s", client.getCurrentUserId(), chosenNumber, betAmount);
+                String response = client.sendRequest("ROLL_DICE:" + requestData);
+                resultLabel.setText("<html><center>" + response.replace(", ", "<br>") + "</center></html>");
+                Timer timer = new Timer(8000, e -> {
+                    resultLabel.setText("Enter your bet amount and choose a number (1-6).");
+                    enableButtons();
+                });
+                timer.setRepeats(false);
+                timer.start();
+            } else {
+                resultLabel.setText("Invalid bet amount entered. Please enter a positive number.");
+            }
+        } else {
+            resultLabel.setText("Please log in to play the game.");
+        }
     }
 
     // Image Rotator Pointer Method
